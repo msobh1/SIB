ae7f4c2f508c329b8ddf18439960c4f2852e58d3
Mahmoud Sobh <msobh71@gmail.com>
2023-05-19 17:11:04 +0200

    l

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import './App.css';
 import Login from './pages/login';
 import Register from './pages/register';
 import Admin from './pages/admin';
