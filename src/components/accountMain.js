1fb3e00cc715c5b3dc65c327dfa43c9ee390d372
Mahmoud Sobh <msobh71@gmail.com>
2023-05-27 16:19:12 +0200

    ee

--- src/components/accountMain.js
+++ src/components/accountMain.js
@@ -1,10 +1,19 @@
 import React from "react";
 import "./accountMainstyles.css"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSack } from "@fortawesome/free-solid-svg-icons";
 
 function AccountMain(){
     return(
         <div className="account-main">
-            hababe
+            <div className="accbalance">
+                <i className="fa-solid fa-sack"></i>
+                <hh>Account ID: <br/> <pp2>1234 5678 8911 4576</pp2> </hh>
+                <br/>
+                <hh>Balance:<br/> <pp2>$ 50000</pp2> </hh>
+            </div>
+            <div className="transfer">
+            </div>
         </div>
     );
 }
