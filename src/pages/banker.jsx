35df7bc5f4c9cadc952827f02e163602446a1ceb
Mahmoud Sobh <msobh71@gmail.com>
2023-05-29 18:38:31 +0200

    sasa

--- src/pages/banker.jsx
+++ src/pages/banker.jsx
@@ -1,9 +1,25 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Banker() {
   return (
     <div>
-      <h1>Banker</h1>
+      <h1>Welcome, Banker!</h1>
+
+      <section>
+        <h2>Loan Requests</h2>
+        <Link to="loanRequests">View Loan Requests</Link>
+      </section>
+
+      <section>
+        <h2>Credit Card Requests</h2>
+        <Link to="cardRequests">View Credit Card Requests</Link>
+      </section>
+
+      <section>
+        <h2>Accounts Management</h2>
+        <Link to="manageAccounts">Manage Accounts</Link>
+      </section>
     </div>
   )
 }
